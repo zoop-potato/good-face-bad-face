f77c35759c5122425e947252b291407524a848fd
zoop-potato <joshbutler2084@gmail.com>
2023-07-20 19:37:51 -0400

    Setup window system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        resolution: (1280., 720.).into(),
+                        title: "Good Face Bad Face".to_string(),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
+        .add_systems(Startup, setup_camera)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
